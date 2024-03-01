56bebb05c9b4b27602f3b06725941149e462e7ec
Marco Gomes <marco@mgsantos.com.br>
2024-03-01 06:59:43 -0300

    Add and Configure Kubernetes Provider

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "oidc" {
   value = module.eks_cluster.oidc
 }
+
+output "certificate_authority" {
+  value = module.eks_cluster.certificate_authority
+}
+
+output "endpoint" {
+  value = module.eks_cluster.endpoint
+}
